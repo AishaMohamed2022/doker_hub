80daa500e551745b65aa73c8ef2138c9a57822a4
AishaMohamed2022 <107565892+AishaMohamed2022@users.noreply.github.com>
2026-09-12 21:38:10 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+#from flask import Flask, jsonify
+# import platform
+# import os
+
+# app = Flask(__name__)   req  Flask==3.0.3
+
+
+
+# @app.route("/")
+# def home():
+#     return jsonify({
+#         "message": "Hello from the Python app!",
+#         "python_version": platform.python_version(),
+#         "hostname": platform.node(),
+#     })
+
+
+# @app.route("/health")
+# def health():
+#     return jsonify({"status": "ok"})
+
+
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host="0.0.0.0", port=port)
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
